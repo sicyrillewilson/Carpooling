7b5a1d8b697fea15b8d52c4eafec605f729e822b
sicyr <sicyrillewilson@gmail.com>
2025-01-24 01:07:14 +0000

    sicyr1

--- CarPooling/app/build.gradle.kts
+++ CarPooling/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:latest_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
