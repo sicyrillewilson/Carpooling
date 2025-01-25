ec445fcab319f225494ab8de5857d44b1d37740a
sicyr <sicyrillewilson@gmail.com>
2025-01-25 07:55:08 +0000

    sicyr6

--- CarPooling/app/src/main/java/tg/eplcoursandroid/carpooling/SignUpActivity.kt
+++ CarPooling/app/src/main/java/tg/eplcoursandroid/carpooling/SignUpActivity.kt
@@ -21,9 +21,10 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var pd : ProgressDialog
     lateinit var auth : FirebaseAuth
     lateinit var firestore : FirebaseFirestore
-    lateinit var name: String
+    //lateinit var name: String
     lateinit var email: String
     lateinit var password: String
+    lateinit var passwordConfirm: String
 
 
 
@@ -40,31 +41,39 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(Intent(this, SignInActivity::class.java))
         }
         binding.signUpValider.setOnClickListener {
-            name = binding.signUpNom.text.toString()
+            //name = binding.signUpNom.text.toString()
             email = binding.signUpEmail.text.toString()
             password = binding.signUpMotDePasse.text.toString()
-            if (binding.signUpNom.text.isEmpty()){
-                Toast.makeText(this, "Enter Votre nom", Toast.LENGTH_SHORT).show()
-            }
+            passwordConfirm = binding.signUpConfirmerMotDePasse.text.toString()
+//            if (binding.signUpNom.text.isEmpty()){
+//                Toast.makeText(this, "Enter Votre nom", Toast.LENGTH_SHORT).show()
+//            }
             if (binding.signUpEmail.text.isEmpty()){
                 Toast.makeText(this, "Enter votre email", Toast.LENGTH_SHORT).show()
             }
             if (binding.signUpMotDePasse.text.isEmpty()){
                 Toast.makeText(this, "Enter votre mot de pasase", Toast.LENGTH_SHORT).show()
             }
-            if (binding.signUpNom.text.isNotEmpty() && binding.signUpEmail.text.isNotEmpty() && binding.signUpMotDePasse.text.isNotEmpty()){
-                createAnAccount(name, password, email)
+            if (binding.signUpConfirmerMotDePasse.text.isEmpty() || !binding.signUpMotDePasse.text.equals(binding.signUpConfirmerMotDePasse.text)){
+                Toast.makeText(this, "Veillez confirmer votre mot de pasase", Toast.LENGTH_SHORT).show()
+            }
+            //if (binding.signUpNom.text.isNotEmpty() && binding.signUpEmail.text.isNotEmpty() && binding.signUpMotDePasse.text.isNotEmpty()){
+            if (binding.signUpEmail.text.isNotEmpty() && binding.signUpMotDePasse.text.isNotEmpty() && binding.signUpConfirmerMotDePasse.text.isNotEmpty() && binding.signUpMotDePasse.text.equals(binding.signUpConfirmerMotDePasse.text)){
+                //createAnAccount(name, password, email)
+                createAnAccount(password, email)
             }
         }
     }
 
-    private fun createAnAccount(name: String, password: String, email: String) {
+//    private fun createAnAccount(name: String, password: String, email: String) {
+    private fun createAnAccount(password: String, email: String) {
         pd.show()
         pd.setMessage("Enregistrement de l'utilisateur")
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {task->
             if (task.isSuccessful){
                 val user = auth.currentUser
-                val dataHashMap = hashMapOf("userid" to user!!.uid!!, "username" to name, "useremail" to email, "status" to "default",
+                //val dataHashMap = hashMapOf("userid" to user!!.uid!!, "username" to name, "useremail" to email, "status" to "default",
+                val dataHashMap = hashMapOf("userid" to user!!.uid!!, "useremail" to email, "status" to "default",
                     "imageUrl" to "https://www.pngarts.com/files/6/User-Avatar-in-Suit-PNG.png")
                 firestore.collection("Users").document(user.uid).set(dataHashMap)
                 pd.dismiss()
