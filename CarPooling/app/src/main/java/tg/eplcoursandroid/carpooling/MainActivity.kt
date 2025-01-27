9dc4cb8e912b775106e3350181b04ad91501dd18
sicyr <sicyrillewilson@gmail.com>
2025-01-27 10:07:05 +0000

    sicyr8

--- CarPooling/app/src/main/java/tg/eplcoursandroid/carpooling/MainActivity.kt
+++ CarPooling/app/src/main/java/tg/eplcoursandroid/carpooling/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import tg.eplcoursandroid.carpooling.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bouton_navigation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Définir l'action sur les éléments de navigation
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Charger un fragment par défaut
+        bottomNavigationView.selectedItemId = R.id.nav_home
+
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
